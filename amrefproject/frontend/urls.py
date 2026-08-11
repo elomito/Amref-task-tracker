95b6f04a6427564d3faf78ae2ac19a24ed648df4
elomito <omitolizatieno@gmail.com>
2026-08-12 02:31:52 +0300

    create urls.py

---
+++ amrefproject/frontend/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("tasks/create/", views.create_task, name="create_task"),
+    path("tasks/<int:task_id>/update/", views.update_task_status, name="update_task"),
+    path("tasks/<int:task_id>/delete/", views.delete_task, name="delete_task"),
+]
