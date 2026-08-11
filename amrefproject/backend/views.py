32dd56bbba4d327aaabb94fbddcdd67a763edbe0
elomito <omitolizatieno@gmail.com>
2026-08-12 01:04:52 +0300

    create views.py

--- amrefproject/backend/views.py
+++ amrefproject/backend/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    Provides list, create, retrieve, update, partial_update, destroy
+    for Task, all wired automatically by the router in urls.py.
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
